530784046c7d2dc4ae7049b067f9b85841201bff
Влад Харитонов <v.kharitonov@solarl.ru>
2021-12-17 09:00:11 +0300

    Добавил защиту роутов с passport.js

--- controllers/action.js
+++ controllers/action.js
@@ -1,4 +1,6 @@
-module.exports.getAll = function (req, res) {};
+module.exports.getAll = function (req, res) {
+	res.json({ message: "actions" });
+};
 module.exports.create = function (req, res) {};
 module.exports.delete = function (req, res) {};
 module.exports.update = function (req, res) {};
--- routes/action.js
+++ routes/action.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const controller = require("../controllers/action");
+const passport = require("passport");
 const router = express.Router();
 
-router.get("/", controller.getAll);
+router.get("/", passport.authenticate("jwt", { session: false }), controller.getAll);
 router.post("/", controller.create);
 router.delete("/:id", controller.delete);
 router.patch("/:id", controller.update);
